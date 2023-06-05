dadcc5a6aae802e59e4c32f5027150b91bc25f4b
Duchnikovsky <filipduchnik@outlook.com>
2023-06-05 20:56:00 +0200

    First config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import CSS from './page.module.css'
+import Image from "next/image";
+import logo from '../assets/logo.png'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className={CSS.main}>
+      <div className={CSS.header}>
+        <Image src={logo} alt='logo' className={CSS.logo}/>
+      </div>
+      <div className={CSS.content}>
 
+      </div>
     </main>
   )
 }
