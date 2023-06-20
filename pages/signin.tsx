fa88bb34427bb96a076ccb8b7e7aa43159b1302e
Duchnikovsky <filipduchnik@outlook.com>
2023-06-20 13:07:29 +0200

    Checking signin error2

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -17,28 +17,28 @@ export default function Signin() {
   const [formDisabled, setFormDisabled] = useState(false)
   const [noti, setNoti] = useState('')
 
-  const inputs = [
-    {
-      id: 1,
-      type: 'email',
-      name: 'email',
-      label: 'E-mail',
-      error: 'Email should match email pattern',
-      pattern: '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$',
-      maxlenght: 50,
-      icon: faUser
-    },
-    {
-      id: 2,
-      type: 'password',
-      name: 'password',
-      label: 'Password',
-      error: 'Password should be 8-18 characters of letters and numbers',
-      pattern: '^[A-Za-z0-9]{8,18}$',
-      maxlenght: 18,
-      icon: faKey
-    }
-  ]
+  // const inputs = [
+  //   {
+  //     id: 1,
+  //     type: 'email',
+  //     name: 'email',
+  //     label: 'E-mail',
+  //     error: 'Email should match email pattern',
+  //     pattern: '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$',
+  //     maxlenght: 50,
+  //     icon: faUser
+  //   },
+  //   {
+  //     id: 2,
+  //     type: 'password',
+  //     name: 'password',
+  //     label: 'Password',
+  //     error: 'Password should be 8-18 characters of letters and numbers',
+  //     pattern: '^[A-Za-z0-9]{8,18}$',
+  //     maxlenght: 18,
+  //     icon: faKey
+  //   }
+  // ]
 
   // async function submitHandler(e:any){
   //   e.preventDefault()
@@ -84,29 +84,31 @@ export default function Signin() {
   // }
 
   return (
-    <div className={CSS.main}>
-      <div className={CSS.header}>
-        <Image src={logo} alt='logo' className={CSS.logo} priority={true} onClick={() => router.push('/')} />
-      </div>
-      <form>
-        {
-          inputs.map((e, index) => (
-            <div key={index} className={CSS.inputBox}>
-              <span className={CSS.label}>{e.label}</span><br></br>
-              <input type={e.type} name={e.name} value={values[e.name]} required pattern={e.pattern} maxLength={e.maxlenght} className={CSS.input} onChange={(e) => setValues({ ...values, [e.target.name]: e.target.value })}></input>
-              <i><FontAwesomeIcon icon={e.icon} /></i>
-              <div className={CSS.inputError}>
-                <span>{e.error}</span>
-              </div>
-            </div>
-          ))
-        }
-        {formDisabled ? <button className={CSS.button} disabled><FontAwesomeIcon icon={faRotate} spin className={CSS.spin} /></button> : <button className={CSS.button}>Sign in</button>}
-      </form>
-      <div className={CSS.redirection}>Don't have account? <b className={CSS.hover}><Link href='/signin' className={CSS.link}>sign up</Link></b></div>
-      {noti.length > 0 && <>
-        {noti === "Successfully logged in" ? <div><FontAwesomeIcon icon={faCircleCheck} beat className={CSS.icon} /><div className={CSS.noti}>{noti}</div></div> : <div><FontAwesomeIcon icon={faCircleInfo} beat className={CSS.icon} /><div className={CSS.noti}>{noti}</div></div>}
-      </>}
-    </div>
+    <>
+    </>
+    // <div className={CSS.main}>
+    //   <div className={CSS.header}>
+    //     <Image src={logo} alt='logo' className={CSS.logo} priority={true} onClick={() => router.push('/')} />
+    //   </div>
+    //   <form>
+    //     {
+    //       inputs.map((e, index) => (
+    //         <div key={index} className={CSS.inputBox}>
+    //           <span className={CSS.label}>{e.label}</span><br></br>
+    //           <input type={e.type} name={e.name} value={values[e.name]} required pattern={e.pattern} maxLength={e.maxlenght} className={CSS.input} onChange={(e) => setValues({ ...values, [e.target.name]: e.target.value })}></input>
+    //           <i><FontAwesomeIcon icon={e.icon} /></i>
+    //           <div className={CSS.inputError}>
+    //             <span>{e.error}</span>
+    //           </div>
+    //         </div>
+    //       ))
+    //     }
+    //     {formDisabled ? <button className={CSS.button} disabled><FontAwesomeIcon icon={faRotate} spin className={CSS.spin} /></button> : <button className={CSS.button}>Sign in</button>}
+    //   </form>
+    //   <div className={CSS.redirection}>Don't have account? <b className={CSS.hover}><Link href='/signin' className={CSS.link}>sign up</Link></b></div>
+    //   {noti.length > 0 && <>
+    //     {noti === "Successfully logged in" ? <div><FontAwesomeIcon icon={faCircleCheck} beat className={CSS.icon} /><div className={CSS.noti}>{noti}</div></div> : <div><FontAwesomeIcon icon={faCircleInfo} beat className={CSS.icon} /><div className={CSS.noti}>{noti}</div></div>}
+    //   </>}
+    // </div>
   )
 }
