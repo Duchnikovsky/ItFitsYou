49c6e4595a707e960e5775ee8c66fe9218f4d554
Duchnikovsky <filipduchnik@outlook.com>
2025-04-10 21:04:43 +0200

    refactor: move PaT structure from routing dir to components

---
+++ src/components/auth/PrivacyAndTerms/index.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { useCallback, useEffect, useState } from "react";
+import style from "./PaT.module.css";
+import Image from "next/image";
+import logo from "@/assets/images/logo.png";
+import cs from "classnames";
+import { LegalContent } from "./LegalContent/LegalContent";
+import Link from "next/link";
+
+const PrivacyAndTerms = () => {
+    const [activeLink, setActiveLink] = useState<"Privacy" | "Terms">(
+        "Privacy"
+    );
+
+    useEffect(() => {
+        const hash = window.location.hash;
+        setActiveLink(hash === "#Terms" ? "Terms" : "Privacy");
+    }, []);
+
+    const handleLinkChange = useCallback((link: "Privacy" | "Terms") => {
+        setActiveLink(link);
+        window.location.hash = link;
+    }, []);
+
+    return (
+        <div className={style.PaT_wrapper}>
+            <div className={style.header}>
+                <Link href="/">
+                    <Image src={logo} alt="logo" width={48} />
+                </Link>
+                <h2>Legal Documents</h2>
+            </div>
+            <div className={style.PaT}>
+                <ul className={style.headerLinks}>
+                    <li
+                        className={cs(style.link, {
+                            [style.active]: activeLink === "Privacy",
+                        })}
+                        onClick={() => handleLinkChange("Privacy")}
+                    >
+                        Privacy Policy
+                    </li>
+                    <li
+                        className={cs(style.link, {
+                            [style.active]: activeLink === "Terms",
+                        })}
+                        onClick={() => handleLinkChange("Terms")}
+                    >
+                        Terms of Service
+                    </li>
+                </ul>
+                <LegalContent activeLink={activeLink} />
+            </div>
+        </div>
+    );
+};
+
+export { PrivacyAndTerms };
